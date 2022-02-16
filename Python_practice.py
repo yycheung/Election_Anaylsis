a55eeb8dc95ccb95a7b03976006cea9305a968ae
yycheung <yycheung@Galaxy.local>
2022-02-15 23:22:12 -0800

    deleted a py file

--- Python_practice.py
+++
@@ -1,4 +0,0 @@
-voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
-
-for i in range(len(voting_data)):
-    print(f"{voting_data[i]['county']} county has {voting_data[i]['registered_voters']:,} registered votors.")
